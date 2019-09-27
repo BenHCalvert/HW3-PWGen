cf368a0c179eebef1f00b35474d1ecc2269d02ad
Ben Calvert <benhcalvert@gmail.com>
2019-09-27 17:41:34 -0600

    updated variable list to include different charecter types and buttons

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,35 @@
 // Set Variables
-var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+var charactersLet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var charactersNum = '0123456789';
+var charactersSpec = '!@#$%^&*()';
 var pwLen = prompt("");
+var GenerateBtn = document.getElementById('btn btn-danger');
+var OutputBox = document.getElementById('pwOutput');
 
 
 // Generate string
 function randomCharacters(letters) {
 var result = '';
 for (var i = 0; i < letters; i++) {
-    result += characters[Math.floor(Math.random() * characters.length)];
+    result += charactersLet[Math.floor(Math.random() * charactersLet.length)];
 }
 return result;
+// this next line doesnt work!!!
+OutputBox.textContent = result;
 }
 // Console log the result
 console.log(randomCharacters(pwLen));
 
+// add result to output box
 
-// Bonus version pushing each random character into an array and joining each into a string
+
+
+// pushing each random character into an array and joining each into a string
 function randomCharactersBonus(letters) {
 var newChars = [];
 for (var i = 0; i < letters; i++) {
-    newChars.push(characters[Math.floor(Math.random() * characters.length)]);
+    newChars.push(charactersLet[Math.floor(Math.random() * charactersLet.length)]);
 }
-return newChars.join('');
+    return newChars.join('');
 }
 console.log(randomCharactersBonus(pwLen));
