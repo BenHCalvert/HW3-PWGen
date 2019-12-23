79b324218d52fa4a59d3da7bf7e67f22504b0d2a
Ben Calvert <benhcalvert@gmail.com>
2019-12-23 06:41:28 -0700

    added if statements for adding char types to FinalPW array

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,12 @@
 // Set Variables
-var charactersLet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+var charactersLet = 'abcdefghijklmnopqrstuvwxyz';
+var charactersCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var charactersNum = '0123456789';
 var charactersSpec = '!@#$%^&*()';
 var GenerateBtn = document.getElementById('btn btn-danger');
 var OutputBox = document.getElementById('pwOutput');
 // Set variable with final PW for pushing to page
-var FinalPW = PWGen(pwLen);
+var FinalPW = ['abcdefghijklmnopqrstuvwxyz']
 
 // create variables for criteria selection toggle buttons
 var specChar = true;
@@ -25,7 +26,7 @@ var generateBtn = document.getElementById("generate");
 
 // change variable based on user input
 
-    // special charecters
+ // special charecters
 specNBtn.onclick = function(){
     let specChar = false;
     console.log('Special Charecters', specChar);
@@ -34,7 +35,8 @@ specYBtn.onclick = function(){
     let specChar = true;
     console.log('Special Charecters', specChar);
 };
-    // capital charecters
+ 
+// capital charecters
 capNBtn.onclick = function(){
     let capChar = false;
     console.log('Cap Charecters', capChar);
@@ -43,7 +45,8 @@ capYBtn.onclick = function(){
     let capChar = true;
     console.log('Cap Charecters', capChar);
 };
-    // numeric charecters
+ 
+// numeric charecters
 numNBtn.onclick = function(){
     let numChar = false;
     console.log('Num Charecters', numChar);
@@ -59,60 +62,79 @@ function getPWLen() {
     console.log('PW Length', pwLen);
 }
 
-    // On click for generate button
+// On click for generate button
 generateBtn.onclick = function(){
     getPWLen();
 };
 
-// Generate string - Characters ************************************************
-function randomCharacters(letters) {
-var result = '';
-for (var i = 0; i < letters; i++) {
-    result += charactersLet[Math.floor(Math.random() * charactersLet.length)];
-}
-return result;
-}
-// Console log the result
-console.log(randomCharacters(pwLen));
-
-// Generate string - Spec Char ************************************************
-function randomNum(letters) {
-    var result = '';
-    for (var i = 0; i < letters; i++) {
-        result += charactersNum[Math.floor(Math.random() * charactersNum.length)];
-    }
-    return result;
-}
-// Console log the result
-console.log(randomNum(pwLen));
-
-// Generate string - Numbers ************************************************
-function randomSpec(letters) {
-    var result = '';
-    for (var i = 0; i < letters; i++) {
-        result += charactersSpec[Math.floor(Math.random() * charactersSpec.length)];
-    }
-    return result;
+// If statements that push different values into the array based on user input toggle switches
+if (specChar === true) {
+    FinalPW.push(charactersSpec);
+    console.log(FinalPW);
 }
 
-// Console log the result
-console.log(randomSpec(pwLen));
-console.log(randomNum(pwLen) + randomSpec(pwLen) + randomCharacters(pwLen));
-
-
-// pushing each random character into an array, then join to a string
-function PWGen(letters) {
-var newChars = [];
-for (var i = 0; i < letters; i++) {
-    newChars.push(charactersLet[Math.floor(Math.random() * charactersLet.length)]);
-    newChars.push(charactersSpec[Math.floor(Math.random() * charactersLet.length)]);
-    newChars.push(charactersNum[Math.floor(Math.random() * charactersLet.length)]);
-}
-    return newChars.join('');
+if (capChar === true) {
+    FinalPW.push(charactersCap);
+    console.log(FinalPW);
 }
 
-// console log array result
-console.log(PWGen(pwLen));
+if (numChar === true) {
+    FinalPW.push(charactersNum);
+    console.log(FinalPW);
+}
+
+
+
+
+// // Generate string - Characters ************************************************
+// function randomCharacters(letters) {
+// var result = '';
+// for (var i = 0; i < letters; i++) {
+//     result += charactersLet[Math.floor(Math.random() * charactersLet.length)];
+// }
+// return result;
+// }
+// // Console log the result
+// console.log(randomCharacters(pwLen));
+
+// // Generate string - Spec Char ************************************************
+// function randomNum(letters) {
+//     var result = '';
+//     for (var i = 0; i < letters; i++) {
+//         result += charactersNum[Math.floor(Math.random() * charactersNum.length)];
+//     }
+//     return result;
+// }
+// // Console log the result
+// console.log(randomNum(pwLen));
+
+// // Generate string - Numbers ************************************************
+// function randomSpec(letters) {
+//     var result = '';
+//     for (var i = 0; i < letters; i++) {
+//         result += charactersSpec[Math.floor(Math.random() * charactersSpec.length)];
+//     }
+//     return result;
+// }
+
+// // Console log the result
+// console.log(randomSpec(pwLen));
+// console.log(randomNum(pwLen) + randomSpec(pwLen) + randomCharacters(pwLen));
+
+
+// // pushing each random character into an array, then join to a string
+// function PWGen(letters) {
+// var newChars = [];
+// for (var i = 0; i < letters; i++) {
+//     newChars.push(charactersLet[Math.floor(Math.random() * charactersLet.length)]);
+//     newChars.push(charactersSpec[Math.floor(Math.random() * charactersLet.length)]);
+//     newChars.push(charactersNum[Math.floor(Math.random() * charactersLet.length)]);
+// }
+//     return newChars.join('');
+// }
+
+// // console log array result
+// console.log(PWGen(pwLen));
 
 // put password in Output box
 OutputBox.textContent = FinalPW;
@@ -123,8 +145,3 @@ function copy() {
     copyText.select();
     document.execCommand("copy");
   }
-
-//   New PW when generate button is clicked
-// document.querySelector('.btn btn-danger').onclick = function(){
-//     PWGen()
-// }
